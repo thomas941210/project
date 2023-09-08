3c3cf89adaeaa25df4d0779ea1d99f98b6287ada
jktpgur <jktpgur@naver.com>
2023-09-08 10:59:13 +0900

    포트폴리오 ver8

---
+++ project4_lexus/js/style.js
@@ -0,0 +1,38 @@
+﻿
+// 1) slider 관련
+$(function(){
+	var mainSwiper=new Swiper(".mainSwiper", {
+		// deleted
+		/*
+		navigation: {
+			nextEl: ".mainSwiper .swiper-button-next",
+			prevEl: ".mainSwiper .swiper-button-prev"
+		},
+		*/
+
+		loop: true, // added
+		autoplay: { // added
+			delay: 5000
+		},
+
+		pagination: {
+			el: ".mainSwiper .swiper-pagination"
+		},
+
+		/*
+		pagination: {
+			el: ".mainSwiper .swiper-pagination",
+			type: "fraction"
+		}
+		*/
+	});
+
+	$(".main_slider .prev").click(function(e){ // added
+		e.preventDefault();
+		mainSwiper.slidePrev();
+	});
+	$(".main_slider .next").click(function(e){ // added
+		e.preventDefault();
+		mainSwiper.slideNext();
+	});
+});
